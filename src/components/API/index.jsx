51c4d25189e754a77fbb2f67bb7d893c8ada1b64
Pedro Gabriel da Rocha Matos <pedrogabriel_r@hotmail.com>
2025-05-26 13:08:45 -0300

    base URL

--- src/components/API/index.jsx
+++ src/components/API/index.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 const API = axios.create({
     //alterar para o endereço do gateway da sua máquina
     //          IP                    PORTA GATEWAY + micro principal
-    baseURL:'http://192.168.15.34' + ':8082' +'/olympics-medal-table-ms'
+    baseURL:'http://192.168.15.189' + ':8082' +'/olympics-medal-table-ms'
 });
 export default API;
 //http://192.168.15.34:8082/ - casa de Pedro
